909330c994d80bbf755c106f446fd3fe8eeaa821
ArshadKhan-UOIT <arshad.khan@ontariotechu.net>
2022-11-19 23:07:44 -0500

    fixing output formatting

--- youtube-link-download.py
+++ youtube-link-download.py
@@ -13,6 +13,12 @@ if (len(sys.argv) == 2):
         exit()
     print("[+] Link connection success!")
 
+# video = yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
+# print(video.mime_type)
+# extension = video.mime_type
+# print(extension.split("/"))
+# print(extension.split("/")[0])
+# print(extension.split("/")[1])
 # print(yt.streams.filter(progressive=True).all())
 # print()
 # print(yt.streams.filter(progressive=True).order_by('resolution').desc().all())
@@ -21,17 +27,20 @@ if (len(sys.argv) == 2):
 # print(yt.streams.filter(adaptive=True,file_extension='mp4').order_by('resolution').desc().first())
 
     try:
-        yt.streams.filter(
-            progressive=True,
-            file_extension='mp4'
-            ).order_by('resolution').desc().first().download()
+        video = yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
+        video.download()
+        # yt.streams.filter(
+        #     progressive=True,
+        #     file_extension='mp4'
+        #     ).order_by('resolution').desc().first().download()
         # yt.streams.filter(adaptive=True,file_extension='mp4').order_by('resolution').desc().first().download() # bug video only, no audio
         # could potentially download video and audio seperately and combine with ffmpeg
     except:
         print("[-] Download failed")
         exit()
-    print("[+] Youtube video '{}' downloaded!".format(yt.title))
-    print("\tVideo saved in directory path: '{}/{}'".format(pathlib.Path(__file__).parent.resolve(),yt.title))
+    extension = video.mime_type
+    print("[+] Youtube video '{}.{}' downloaded!".format(video.title,extension.split("/")[1]))
+    print("\tVideo saved in directory path: '{}/{}.{}'".format(pathlib.Path(__file__).parent.resolve(),video.title,extension.split("/")[1]))
 else:
     print("[-] Usage: python3 <python_file_name> <youtube video link>")
     exit()
